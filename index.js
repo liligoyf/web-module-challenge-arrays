38c8b5afc9324831b8810b329fabd83bf0859435
lili <jordanfuchs05@yahoo.com>
2020-10-07 21:39:00 -0700

    finished project

--- index.js
+++ index.js
@@ -67,8 +67,9 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(arr){
+    return arr.length === 31
+   
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -84,8 +85,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+function addFlavor(arr,newFlavor){
+    arr.unshift(newFlavor)
+    return arr 
 }
 
 
@@ -100,8 +102,10 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
+function removeLastFlavor(arr){
+   arr.pop()
+   return arr
+
 }
 
 
@@ -116,8 +120,8 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(arr,index){
+    return arr[index]
 }
 
 
@@ -135,8 +139,9 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(arr,flavor){
+    arr.splice(arr.indexOf(flavor),1)
+    return arr
 }
 
 
@@ -160,8 +165,16 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(arr,flavor){
+    let newarr = []
+    for(let i = 0; i <arr.length; i++){
+        if(arr[i].includes(flavor)){
+            newarr.push(arr[i])
+
+        }
+    }
+    return newarr;
+    
 }
 
 
